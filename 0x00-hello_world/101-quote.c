82d9d081ca409a71512dbf7d8fcb4d5875a76708
Joelon <joelonjohnson@gmail.com>
2022-12-08 13:21:41 -0800

    scritp to display text

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Using char string
+ */
+int main(void)
+{
+char str0[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+write(2, str0, 59);
+return (1);
+}
