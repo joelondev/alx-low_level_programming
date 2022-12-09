71619718200c5d15834c999cafad758691681a40
Joelon <joelonjohnson@gmail.com>
2022-12-09 15:49:10 -0800

    added single digits solution

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - program prints all digit numbers  of base 10
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
