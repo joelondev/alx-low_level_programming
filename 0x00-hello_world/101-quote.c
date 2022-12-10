7cb0154463ac0a343fed1a34f15f4549c2393467
Joelon <joelonjohnson@gmail.com>
2022-12-10 03:20:01 -0800

    added and fixed error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-char str0[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", str0[]);
-return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19",
+	 sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	return (1);
 }
