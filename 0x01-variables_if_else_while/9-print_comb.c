a90435bea10774f12016af7073f59e2a40b35e0b
Joelon <joelonjohnson@gmail.com>
2022-12-09 16:58:27 -0800

    afixed errors

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,11 +13,11 @@ int main(void)
 	for (q = '0'; q < '9'; q++)
 	{
 		putchar(q);
-	if (q != '9')
-	{
-       		putchar(',');
-		putchar(' ');
-	}
+		if (q != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
