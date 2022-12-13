81b956dda0725820bf46c9b0569c0d925376419d
Joelon <joelonjohnson@gmail.com>
2022-12-13 13:30:04 -0800

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an integer
+ * @q: int datatype number
+ * Return: value
+ */
+
+int _abs(int q)
+{
+	if (q < 0)
+	{
+		return(n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
