2a8312005d61a553ce85308bd41e0e010593651b
Joelon <joelonjohnson@gmail.com>
2022-12-13 12:01:53 -0800

    added description for c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * _islower - program checks for lowercase character
+ * _islower - program checks for lowercase character.
+ * c: char type
+ * Return: 1
  */
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
