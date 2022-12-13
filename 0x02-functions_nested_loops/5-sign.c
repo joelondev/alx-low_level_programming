6a6550c649aebb0b11e53edd6abdac91ec7bab63
Joelon <joelonjohnson@gmail.com>
2022-12-13 12:55:58 -0800

    negative and positive

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_sign - prints th sign opf a number
+ * @c: integer could be +ve or -ve
+ * Return: 1
+ */
+
+int print_sign(int c)
+{
+	if (c > 0)
+		_putchar('+');
+	return (1);
+	else if (c < 0)
+		_putchar('-');
+	return (-1);
+	else
+		_putchar('0');
+	return (0);
+}
