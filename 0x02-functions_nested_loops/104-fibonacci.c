3ce4238de0a8125df11ab2db487808486aac190a
Joelon <joelonjohnson@gmail.com>
2022-12-14 04:50:13 -0800

    added file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Description: This program finds and prints the first 98
+ * Fibonacci numbers, starting with 1 and 2 and a new line
+ */
+int main(void)
+{
+	
