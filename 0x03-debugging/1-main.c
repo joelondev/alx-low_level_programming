aee4c0fcd75ced2b5ebbfc0bb2059fecbf456d47
Joelon <joelonjohnson@gmail.com>
2022-12-14 15:40:02 -0800

    added second debug

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stido.h>
+
+/**
+ * main - cuases an infinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*while (i < 0)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}
