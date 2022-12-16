268b5b022d2c0e2bfc46b9e376a481b32b7cf30f
Joelon <joelonjohnson@gmail.com>
2022-12-15 18:18:42 -0800

    program prints square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - draws a square
+ * @size: length and width of square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	y = 0;
+
+	if (size < 1)
+		_putchar('\n');
+
+	while (y < size)
+	{
+		x = 0;
+		while (x < size)
+		{
+			_putchar('*');
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
