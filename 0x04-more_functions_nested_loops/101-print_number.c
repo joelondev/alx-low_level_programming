f3414d445b18be12e53584585d39c1e0badf89ca
Joelon <joelonjohnson@gmail.com>
2022-12-15 18:52:33 -0800

    long

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * print_number - Function that prints an integer.
+ * @n: int type number
+ * Description: Can only use _putchar to print.
+ */
+void print_number(int n)
+{
+	long power; /* power of 10 */
+	int bool_c; /* boolean check */
+	long num; /* converting int to long */
+
+	num = n;
+	/* negatives */
+	if (num < 0)
+	{
+		num *= -1;
+		_putchar('-');
+	}
+
+	/* count up */
+	power = 1;
+	bool_c = 1;
+	while (bool_c)
+	{
+		if (num / (power * 10) > 0)
+			power *= 10;
+		else
+			bool_c = 0;
+	}
+
+	/* count down */
+	while (num >= 0)
+	{
+		if (power == 1)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar((num / power % 10) + '0');
+			power /= 10;
+		}
+	}
+}
+Footer
