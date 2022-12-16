9943144fe8ed82553bc58f4becdb55c58f4be054
Joelon <joelonjohnson@gmail.com>
2022-12-15 17:53:58 -0800

    add program to pultiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 integers
+ * @x: int datatype number
+ * @y: int datatype number
+ * Return: result of multiplication
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
