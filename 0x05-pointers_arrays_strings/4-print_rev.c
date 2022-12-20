06ae1b5426d30b16ddab7985f58e653e84f4d9d5
Joelon <joelonjohnson@gmail.com>
2022-12-19 17:09:24 -0800

    space

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,17 +9,16 @@
 void print_rev(char *s)
 {
 	int i = 0;
-	int j;
 
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 	i--;
-	while (j >= 0)
+	while (i >= 0)
 	{
-		_putchar(s[j]);
-		j--;
+		_putchar(s[i]);
+		i--;
 	}
 	_putchar('\n');
 }
