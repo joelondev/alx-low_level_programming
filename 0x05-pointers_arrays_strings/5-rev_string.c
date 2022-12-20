66f09306da870e30a2044ad7284614f5f3307198
Joelon <joelonjohnson@gmail.com>
2022-12-19 17:19:02 -0800

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_rev - reverses a string
+ * @s: pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, mag, half;
+	char first, last;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	mag = i - 1;
+	half = mag / 2;
+	while (half >= 0)
+	{
+		first = s[mag - half];
+		last = s[half];
+		s[half] = first;
+		s[mag - half] = last;
+		half--;
+	}
+}
