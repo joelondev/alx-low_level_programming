35e9e20c1fda8d6caee7fa3fc9011dc87cf04f00
Joelon <joelonjohnson@gmail.com>
2022-12-22 13:56:02 -0800

    space added and removed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,19 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i, j;
+	int i = 0, j = 0;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-		;
-	}
+	while (dest[i] != '\0')
+		i++;
 
-	for (j = 0; src[j] != '\0'; j++)
+	while (src[j] != '\0')
 	{
-		dest[j] = src[j];
+		dest[i] = src[j];
+		i++;
 		j++;
 	}
 
 	dest[i] = '\0';
+
 	return (dest);
 }
