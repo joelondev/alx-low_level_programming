b4b9a5123c31b537dd8374f106fe23d5377d0c49
Joelon <joelonjohnson@gmail.com>
2023-01-03 08:30:17 -0800

    search string

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,20 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int c = 0;
-	char *t = accept;
+	unsigned int i = 0;
+	char *a = accept;
 
 	while (*s++)
 	{
 		while (*accept++)
 			if (*(s - 1) == *(accept - 1))
 			{
-				c++;
+				i++;
 				break;
 			}
 		if (!(*--accept))
 			break;
-		accept = t;
+		accept = a;
 	}
-	return (c);
+	return (i);
 }
