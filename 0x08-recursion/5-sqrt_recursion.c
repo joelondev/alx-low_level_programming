e83df121df42f70c8b575363e38e63290fa95e9f
Joelon <joelonjohnson@gmail.com>
2023-01-05 06:41:18 -0800

    added helper fucntion; square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - func that returns the natural
+ * square root of a number
+ * @n: the number whose square root is to be found
+ * Return: return the natural square root, else return -1
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (help_func(n, 1));
+}
+
+/**
+ * help_func - helper function to solve _sqrt_recursion
+ * @c: number to determine if square root
+ * @i: incrementer to compare with `c`
+ * Return: square root if natural square root, or -1 if none found
+ */
+
+int help_func(int c, int i)
+{
+	int square;
+
+	square = i * i;
+	if (square == c)
+		return (i);
+	else if (square < c)
+		return (help_func(c, i + 1));
+	else
+		return (-1);
+}
