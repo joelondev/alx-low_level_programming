78268ec4ee39e3fdb8f2677b51b816ede522667f
Joelon <joelonjohnson@gmail.com>
2023-01-10 07:55:05 -0800

    add strup file

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory.
+ * @str: string.
+ * Return: pointer of an array of chars
+ */
+
+char *_strdup(char *str)
+{
+	char *strpt;
+	unsigned int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	strpt = (char *)malloc(sizeof(char) * (i + 1));
+
+	if (strpt == NULL)
+		return (NULL);
+
+	for (j = 0; j <= i; j++)
+		strpt[j] = str[j];
+
+	return (strpt);
+}
