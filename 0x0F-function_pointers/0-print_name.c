18cc87c6c5de260b5d27375324cf2b5d59cdb5db
Joelon <joelonjohnson@gmail.com>
2023-01-18 07:55:45 -0800

    update header

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdio.h>
 
 /**
  * print_name - function that prints a name
@@ -10,10 +9,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-	{
-		return;
-	}
-	
 	f(name);
 }
